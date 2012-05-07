18426649e6da535e126559d73883015c0bc279f8
Aron Koszo <aron.koszo@icbtech.rs>
2012-05-07 19:32:59 +0200

    Database related documentation

--- OracleProba/src/com/github/koszoaron/adatb2012/OracleProbaApp.java
+++ OracleProba/src/com/github/koszoaron/adatb2012/OracleProbaApp.java
@@ -2,13 +2,19 @@ package com.github.koszoaron.adatb2012;
 
 import com.github.koszoaron.adatb2012.db.DBService;
 import com.github.koszoaron.adatb2012.pojo.Employee;
+import com.github.koszoaron.adatb2012.pojo.Felhasznalo;
+import com.github.koszoaron.adatb2012.util.DatabaseService;
 
 public class OracleProbaApp {
     public static void main(String[] args) {
-        DBService service = DBService.getInstance();
+        //DBService service = DBService.getInstance();
         
-        for (Employee e : service.getAllEmployees()) {
+        /*for (Employee e : service.getAllEmployees()) {
             System.out.println(e.toString());
-        }
+        }*/
+        
+        DatabaseService service = DatabaseService.getInstance();
+        Felhasznalo user = service.getFelhasznaloByUsername("ARON");
+        System.out.println(user.toString());
     }
 }
